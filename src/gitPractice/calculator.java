35625bdd003432e2c8dae377ae5fa9f5253c98ce
neyuc <nm.yucesoy@gmail.com>
2022-01-18 20:38:09 -0500

    created subtract method

--- src/gitPractice/calculator.java
+++ src/gitPractice/calculator.java
@@ -4,9 +4,15 @@ public class calculator {
     public static void main(String[] args) {
 
     }
-    public static int add(int num1, int num2 ){
-        int result=num1 +num2;
+
+    public static int add(int num1, int num2) {
+        int result = num1 + num2;
         return result;
     }
 
+    public static int subtract(int num1, int num2) {
+        int result = num1 - num2;
+        return result;
+
+    }
 }
