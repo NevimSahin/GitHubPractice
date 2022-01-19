2f3fae21a53c3b37f4894180db166c6775469758
neyuc <nm.yucesoy@gmail.com>
2022-01-18 20:40:54 -0500

    created multply method

--- src/gitPractice/calculator.java
+++ src/gitPractice/calculator.java
@@ -20,4 +20,9 @@ public class calculator {
         int result = num1 / num2;
         return result;
     }
+
+    public static int multpl(int num1, int num2) {
+        int result = num1 * num2;
+        return result;
+    }
 }
